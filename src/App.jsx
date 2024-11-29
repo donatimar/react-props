ed68a5ce053a3c4e4ff3a329e04a6394de9b8616
donatimar <donatimar@gmail.com>
2024-11-29 17:02:32 +0100

    removed image, created props

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,27 @@ import Footer from "./components/Footer";
 import { posts } from "./data/posts";
 
 function App() {
+  const publishedPosts = posts.filter((post) => post.published);
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
-
       <main className="flex-grow-1 bg-light py-5">
-        <div className="container d-flex justify-content-center">
-          <Card />
+        <div className="container">
+          <div className="row">
+            {publishedPosts.map((post) => (
+              <div className="col-4" key={post.id}>
+                <Card
+                  title={post.title}
+                  image={post.image || "https://placehold.co/600x400"}
+                  content={post.content}
+                  tags={post.tags}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </main>
-
       <Footer />
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,39 @@
 import React from "react";
-import image from "../assets/images.png";
 
-function Card() {
+function Card({ title, image, content, tags }) {
+  const tagColors = {
+    html: "success",
+    css: "primary",
+    js: "warning",
+    php: "info",
+  };
+
+  const placeholderImage = "https://placehold.co/600x400";
+
   return (
-    <div className="card" style={{ width: "30rem" }}>
-      <img src={image} className="card-img-top img-fluid" alt="Placeholder" />
+    <div className="card">
+      <img
+        src={image || placeholderImage}
+        className="card-img-top img-fluid"
+        alt={title}
+      />
       <div className="card-body text-start">
-        <h5 className="card-title">Titolo del post</h5>
-        <p className="card-text">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta
-          perferendis cumque numquam illo non ducimus hic veritatis est quae
-          maxime sint quia laudantium nesciunt, iste ipsa magni atque ratione
-          libero?
-        </p>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{content}</p>
+        <div className="mb-3">
+          {tags.map((tag) => (
+            <span
+              key={tag}
+              className={`badge text-white bg-${
+                tagColors[tag] || ""
+              } d-inline-block mb-2 me-2`}
+            >
+              {tag.toUpperCase()}
+            </span>
+          ))}
+        </div>
         <a href="#" className="btn btn-warning text-white">
-          LEGGI DI PIU'
+          LEGGI DI PIÙ
         </a>
       </div>
     </div>
